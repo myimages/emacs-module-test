700969825f01f6e2ee1be7747af0c57c96fba3b8
Ayed <grcc.sa@gmail.com>
2017-11-11 00:06:10 +0300

    uneeded

--- t.el
+++ t.el
@@ -1,12 +1,6 @@
 (require 'ert)
-(require 'cl-lib)
 (require 'module-test)
 
-
-;; (ert-deftest test-calc-score ()
-;;   "file open and close test"
-;;   (let ((score (calc-score "needle" "score the needle in the haystack")))
-;;     (should (= score 0.5739583373069763))))
 
 (ert-deftest test-calc-score ()
   "test score calculator"
