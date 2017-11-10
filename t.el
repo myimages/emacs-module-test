cd832a7046f43e4add845cc8a2aaffba72aafdb0
Ayed <grcc.sa@gmail.com>
2017-11-11 00:06:28 +0300

    clean up

--- t.el
+++ t.el
@@ -3,7 +3,5 @@
 
 
 (ert-deftest test-calc-score ()
-  "test score calculator"
-  ;;573958
   (let ((score (calc-score "needle" "score the needle in the haystack")))
     (should (equal score 0.5739583373069763))))
