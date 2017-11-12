0edc4ad95b83a82ad6607f5abbd22caf4f95bbd6
Ayed <grcc.sa@gmail.com>
2017-11-12 06:31:56 +0300

    case sensative match.

--- t.el
+++ t.el
@@ -5,3 +5,7 @@
 (ert-deftest test-calc-score ()
   (let ((score (calc-score "needle" "score the needle in the haystack")))
     (should (equal score 0.5739583373069763))))
+
+(ert-deftest test-calc-score-case-sensative ()
+  (let ((score (calc-score "Foo" "Foobar" t)))
+    (should (equal score 0.75))))
