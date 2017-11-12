7611cf121e6492a3fae91bbe579de065767e668b
Ayed <grcc.sa@gmail.com>
2017-11-12 06:54:59 +0300

    Revert "to avoid surprises , it takes two args only"
    This reverts commit 4d63f4b0e31e53d2eab85cd66220a6d69f0b4810.

--- t.el
+++ t.el
@@ -3,9 +3,7 @@
 
 
 (ert-deftest test-calc-score ()
-  (let ((score (calc-score "needle" "score the needle in the haystack")))
-    (should (equal score 0.5739583373069763))))
+    (should (= (calc-score "needle" "score the needle in the haystack") 0.5739583373069763)))
 
-(ert-deftest test-calc-score-case-sensative ()
-  (let ((score (calc-score "Foo" "Foobar" t)))
-    (should (equal score 0.75))))
+;; (ert-deftest foo ()
+;;     (should (= (calc-score "Foo" "Foobar" t) 0.75)))
